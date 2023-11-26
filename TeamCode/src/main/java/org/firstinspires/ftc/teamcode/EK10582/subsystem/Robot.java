511a521eee513cdcbeb953705c98b6caaa77fb65
Shining <shinypup06@gmail.com>
2023-11-25 19:40:01 -0500

    added subsystems back

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/EK10582/subsystem/Robot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/EK10582/subsystem/Robot.java
@@ -71,11 +71,11 @@ public class Robot {
 
     //Add all subsystems to a list to be initiated and updated through
     //private List<Subsystem> subsystems = Arrays.asList(mecanumDrive, intake, aprilTags, slides, housing, openCV);
-    public List<Subsystem> subsystems = Arrays.asList(mecanumDrive);
+    public List<Subsystem> subsystems = Arrays.asList(mecanumDrive, intake, openCV, slides, housing);
 
     //add all subsystems that need to go through telemetry
 //    private List<Subsystem> telemetrySubsystems = Arrays.asList();
-    public List<Subsystem> telemetrySubsystems = Arrays.asList(mecanumDrive);
+    public List<Subsystem> telemetrySubsystems = Arrays.asList(mecanumDrive, intake, slides, housing, openCV);
 
 
     //Creates an arraylist called actions that stores all the actions that are currently being done
